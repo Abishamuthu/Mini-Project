6010ccb833b81b8b58e1d38393c4a5d94e93a3e6
Abisha <abishamuthukrishnan2003@gmail.com>
2023-11-13 23:10:46 +0530

    Mini Project

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3c11ba3c5": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
